2bfb586caafac8b12c4200650c3801d406eb9bcf
Juanc4rl0ss <juan_c1985@hotmail.es>
2025-03-26 13:09:15 +0100

    Seguimos con las pruebas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,8 +11,8 @@ import io from 'socket.io-client';
 
 Modal.setAppElement('#root');
 
-const socket = io('http://ip172-18-0-49-cvhu8s291nsg00bavpag-3000.direct.labs.play-with-docker.com');
-const url = 'http://ip172-18-0-49-cvhu8s291nsg00bavpag-3000.direct.labs.play-with-docker.com/api';
+const socket = io('http://backend:3000'); 
+const url = 'http://backend:3000';
 
 const colorPalette = [
   '#1F77B4', '#FF7F0E', '#2CA02C', '#D62728', '#9467BD',
